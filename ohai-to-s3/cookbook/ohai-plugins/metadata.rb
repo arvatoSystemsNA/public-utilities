1b9005dd9c48444484dae0971b35d9b0e3c410cc
Nicholas Hofbauer <nicholas.hofbauer@arvatosystems.com>
2016-02-05 10:51:32 -0500

    Upgrade version to 1.0.1 due to yum changes in the master repo

--- ohai-to-s3/cookbook/ohai-plugins/metadata.rb
+++ ohai-to-s3/cookbook/ohai-plugins/metadata.rb
@@ -3,7 +3,7 @@ maintainer_email  "vmartin@cloudaware.com"
 license           "Private"
 description       "Install custom ohai plugins."
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version           "1.0.0"
+version           "1.0.1"
 recipe            "ohai-plugins", "Install custom ohai plugins."
 name              "ohai-plugins"
 
